0824165c935d79611ce1f196d60577c7b0b270a6
mekson bravo <95582758+haben-tekeste@users.noreply.github.com>
2023-09-23 05:37:58 +0000

    refactored code

--- index.js
+++ index.js
@@ -1,28 +1,71 @@
 const http = require("node:http");
 const PORT = 5255;
 
+const HTTP_METHODS = {
+  GET: "GET",
+  POST: "POST",
+  PUT: "PUT",
+  PATCH: "PATCH",
+  DELETE: "DELETE",
+  TRACE: "TRACE",
+  OPTIONS: "OPTIONS",
+  HEAD: "HEAD",
+};
+
 class Router {
   constructor() {
     // stores our routes
     this.routes = {};
   }
 
-  addRoute(method, path, handler) {
+  #addRoute(method, path, handler) {
     this.routes[`${method} ${path}`] = handler;
-    console.log(this.routes);
   }
 
   printRoutes() {
     console.log(Object.entries(this.routes));
   }
 
+  get(path, handler) {
+    this.#addRoute(HTTP_METHODS.GET, path, handler);
+  }
+
+  post(path, handler) {
+    this.#addRoute(HTTP_METHODS.POST, path, handler);
+  }
+
+  put(path, handler) {
+    this.#addRoute(HTTP_METHODS.PUT, path, handler);
+  }
+
+  delete(path, handler) {
+    this.#addRoute(HTTP_METHODS.DELETE, path, handler);
+  }
+
+  patch(path, handler) {
+    this.#addRoute(HTTP_METHODS.PATCH, path, handler);
+  }
+
+  head(path, handler) {
+    this.#addRoute(HTTP_METHODS.HEAD, path, handler);
+  }
+
+  trace(path, handler) {
+    this.#addRoute(HTTP_METHODS.TRACE, path, handler);
+  }
+
+  options(path, handler) {
+    this.#addRoute(HTTP_METHODS.OPTIONS, path, handler);
+  }
+
   handleRequest(request, response) {
-    console.log(request.url, request.method);
     const { url, method } = request;
     const handler = this.routes[`${method} ${url}`];
 
     if (!handler) {
-      return console.log("404 Not found");
+      console.log("404 Not found");
+      response.writeHead(404, { "Content-Type": "text/plain" });
+      return response.end("Not found");
     }
 
     handler(request, response);
@@ -30,11 +73,13 @@ class Router {
 }
 
 const router = new Router();
-router.addRoute("GET", "/", function handleGetBasePath() {
+router.addRoute("GET", "/", function handleGetBasePath(req, res) {
   console.log("Hello from my GET /");
+  res.end();
 });
-router.addRoute("POST", "/", function handleGetBasePath() {
+router.addRoute("POST", "/", function handleGetBasePath(req, res) {
   console.log("Hello from my POST /");
+  res.end();
 });
 
 http.createServer((req, res) => router.handleRequest(req, res)).listen(PORT);
